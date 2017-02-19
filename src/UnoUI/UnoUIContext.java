323a562d87989230d63120b1e329ad7d611c8761
Alessio Netti <the.shadow.nexus@gmail.com>
2017-02-19 16:24:46 +0100

    Commit test

--- src/UnoUI/UnoUIContext.java
+++ src/UnoUI/UnoUIContext.java
@@ -8,6 +8,8 @@ import com.badlogic.gdx.*;
 
 public class UnoUIContext extends Game {
 
+    public int counter = 0;
+
     public void create() {
         this.setScreen(new UnoUIMain(this));
     }
