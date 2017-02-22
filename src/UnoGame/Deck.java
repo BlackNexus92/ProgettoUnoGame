af3cad7c2b2b3e14fc064c96f2d658214288aac9
Alessio Netti <the.shadow.nexus@gmail.com>
2017-02-22 17:18:13 +0100

    Commit network - 2

--- src/UnoGame/Deck.java
+++ src/UnoGame/Deck.java
@@ -13,6 +13,8 @@ import java.util.Random;
 
 public class Deck implements Serializable {
 
+    private static final long serialVersionUID = 1L;
+
     public static final int DECKSIZE = 108;
     public static final int HANDSIZE = 7;
 
